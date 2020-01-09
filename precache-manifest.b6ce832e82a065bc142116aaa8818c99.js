d2382cc0c9741e4f50a49472db30123766104d0c
yudahadi14 <yudahadi14@gmail.com>
2020-01-09 08:44:31 +0700

    Updates

---
+++ precache-manifest.b6ce832e82a065bc142116aaa8818c99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fb89bb1ca366bb4da903691505cf746",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "3f7788c9829f10792626",
+    "url": "/hello/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0bab7cc8d462de7cb5c6",
+    "url": "/hello/static/js/2.f0290c5b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/hello/static/js/2.f0290c5b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3f7788c9829f10792626",
+    "url": "/hello/static/js/main.29ebd5b1.chunk.js"
+  },
+  {
+    "revision": "9d57a751342914a3f6b4",
+    "url": "/hello/static/js/runtime-main.8e041f5c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello/static/media/logo.5d5d9eef.svg"
+  }
+]);
